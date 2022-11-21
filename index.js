a4a32585120b6d94c92f3c1fbdf1ead5b5930ac7
i-muiri <ianmuiri99@gmail.com>
2022-11-21 12:26:15 +0300

    phaseone intro js

--- index.js
+++ index.js
@@ -5,40 +5,52 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
-
+const pi = 3.142;
+printValue(pi)
 
 //TODO: ADD redeclarable variable
-
+var score = 100;
+var score = 50;
+printValue(score)
 
 //TODO: ADD re-assignable but not redeclarable variable
+let age = 17;
+printValue(age)
 
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
-
-
+const y=24;
+printValue(y)
 //TODO: Create a redeclarable variable with a boolean value
-
-
+var male= true;
+printValue(male)
 //TODO: Create a re-assignable variable with a string value
-
-
+var name = "this is a string";
+printValue(name)
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
-
+const message = "I am in phase 1"
+const time = " 1200hrs";
+const full = message + time;
+printValue(full)
 
 //TODO: Interpolate string (`${}`)
-
+const other = `Hello, ${message} and the time is ${time}`
+printValue(other)
 
 //TODO: Convert to uppercase
-
+const otherVariable = other.toUpperCase()
+printValue(otherVariable)
 
 //TODO: Convert to lowercase
-
+const otherVariable2 = other.toLowerCase()
+printValue(otherVariable2)
 
 //TODO: Index a specific character
-
+const alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+printValue(alphabet.charAt(9))
 
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
